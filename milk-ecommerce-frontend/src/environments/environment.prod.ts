2d0b456af2f7e02acafe837fb331b5911a308a7c
AbidHusain <revasiyaabidhusain786@gmail.com>
2026-03-20 11:09:47 +0530

    Initial frontend commit

--- milk-ecommerce-frontend/src/environments/environment.prod.ts
+++ milk-ecommerce-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
+// export const environment = {
+//   production: true,
+//   apiBaseUrl: 'http://localhost:8080'
+// };
 export const environment = {
-  production: true,
+  production: false,
   apiUrl: 'https://milk-ecommerce-backend-production.up.railway.app/'
 };
--- milk-ecommerce-frontend/src/environments/environment.ts
+++ milk-ecommerce-frontend/src/environments/environment.ts
@@ -1,3 +1,8 @@
+// export const environment = {
+//   production: false,
+//   apiBaseUrl: 'http://localhost:8080'
+  
+// };
 export const environment = {
   production: false,
   apiUrl: 'https://milk-ecommerce-backend-production.up.railway.app/'
